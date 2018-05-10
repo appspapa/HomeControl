b978ed6c62e7a704285c70636663da2135342017
Danny Mak <dannymak@appspapa.com>
2018-05-10 22:13:29 +0800

    Initial Commit

---
+++ HomeControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeControl
+//
+//  Created by Mak Chun Pong Danny on 10/5/2018.
+//  Copyright © 2018 DEVELOPAPA LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

feab7e63051acce28e0b33926a21689612807a1f
Danny Mak <dannymak@appspapa.com>
2018-05-10 22:37:40 +0800

    Add new BT feature.

---
+++ HomeControl/BTFunction.swift
@@ -0,0 +1,9 @@
+//
+//  BTFunction.swift
+//  HomeControl
+//
+//  Created by Mak Chun Pong Danny on 10/5/2018.
+//  Copyright © 2018 DEVELOPAPA LIMITED. All rights reserved.
+//
+
+import Foundation
